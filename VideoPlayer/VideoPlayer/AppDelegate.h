52bf738d393f98b7ebe3067f0c97485cc232b99a
zhuaijun <978206970@qq.com>
2017-08-23 14:55:42 +0800

    最后一次重够，在有下一次孙子

---
+++ VideoPlayer/VideoPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoPlayer
+//
+//  Created by 朱爱俊 on 2017/8/23.
+//  Copyright © 2017年 朱爱俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
